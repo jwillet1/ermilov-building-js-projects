1a5055025419b371eec02df583ee5a48f0ca30f9
Joshua Willett <jwillet1@gmail.com>
2017-03-06 13:51:37 +0000

    update to include testing files and eslint config

---
+++ server/src/app.js
@@ -0,0 +1,31 @@
+// npm packages
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+// our packages
+import {logger} from './util';
+
+// init app
+const app = express();
+
+// setup logging
+app.use(morgan('combined', {stream: logger.stream}));
+
+// add body parsing
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+// test method
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// catch all unhandled errors
+app.use((err, req, res, next) => { // eslint-disable-line
+  logger.error('unhandled application error: ', err);
+  res.status(500).send(err);
+});
+
+// export app
+export default app;
---
+++ server/test/index.js
@@ -0,0 +1,36 @@
+// npm packages
+import test from 'tape';
+import request from 'supertest';
+
+// our packages
+import app from '../src/app';
+
+test('GET /', (t) => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+      const expectedBody = 'Hello world!';
+      const actualBody = res.text;
+
+      t.error(err, 'No error');
+      t.equal(actualBody, expectedBody, 'Retrieve body');
+      t.end();
+    });
+});
+
+test('404 on nonexistant URL', (t) => {
+  request(app)
+    .get('/GETShouldFailOnRandomURL')
+    .expect(404)
+    .expect('Content-Type', /text\/html/)
+    .end((err, res) => {
+      const expectedBody = 'Cannot GET /GETShouldFailOnRandomURL\n';
+      const actualBody = res.text;
+
+      t.error(err, 'No error');
+      t.equal(actualBody, expectedBody, 'Retrieve body');
+      t.end();
+    });
+});
